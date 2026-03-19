fb8d1ca3a235bf3d7d437c7a7317dbf69ed0ef1d
peugeot40-6 <ccocquyt80@gmail.com>
2026-03-19 21:30:56 +0100

    Update app.py

--- app.py
+++ app.py
@@ -106,7 +106,7 @@ def pronostic():
 
     return render_template("pronostic.html", gps=gps, pilotes=pilotes)
     
-🏁 2️⃣ ENCODER LES RÉSULTATS GP
+🏁️⃣  ENCODER LES RÉSULTATS GP
 🎯 Objectif
 
 👉 Encoder les résultats du top 10
@@ -156,7 +156,7 @@ def ajouter_resultat():
     <button type="submit">Valider</button>
 </form>
 
-🏆 3️⃣ CLASSEMENT AUTOMATIQUE
+🏆️⃣ CLA SSEMENT AUTOMATIQUE
 
 👉 on reprend TON système exact (F1 + bonus)
 
