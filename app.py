586fd332f1ef3d839c05b85c12a5cd3c93c8da8f
peugeot40-6 <ccocquyt80@gmail.com>
2026-05-29 21:55:44 +0200

    Add 'Miami' and 'Canada' to COURSES_VERROUILLEES

--- app.py
+++ app.py
@@ -226,7 +226,7 @@ def mes_pronostics():
 
 
 # PRONOSTIC
-COURSES_VERROUILLEES = ["Australie", "Chine", "Japon"]
+COURSES_VERROUILLEES = ["Australie", "Chine", "Japon", "Miami", "Canada"]
 
 @app.route("/pronostic", methods=["GET", "POST"])
 @login_required
