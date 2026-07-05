13dc3012e5c3af119adb9e32e8ec385d6dcb6003
peugeot40-6 <ccocquyt80@gmail.com>
2026-07-05 11:55:48 +0200

    Add 'Barcelone' and 'Autriche' to locked courses

--- app.py
+++ app.py
@@ -226,7 +226,7 @@ def mes_pronostics():
 
 
 # PRONOSTIC
-COURSES_VERROUILLEES = ["Australie", "Chine", "Japon", "Miami", "Canada", "Monaco"]
+COURSES_VERROUILLEES = ["Australie", "Chine", "Japon", "Miami", "Canada", "Monaco", "Barcelone", "Autriche"]
 
 @app.route("/pronostic", methods=["GET", "POST"])
 @login_required
